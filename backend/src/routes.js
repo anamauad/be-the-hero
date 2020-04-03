e873eafa300b9892121e9503f9663b96b98058a0
anamauad <ana.mauad@gmail.com>
2020-04-03 16:07:25 -0300

    Adicionando celebrate para validar o que é passado para os endpoints no backend.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const NgoController = require('./controllers/NgoController');
 const IncidentController = require('./controllers/IncidentController');
@@ -7,16 +8,58 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(),
+  }),
+}), SessionController.create);
+
 
 routes.get('/ngos', NgoController.list);
-routes.post('/ngos', NgoController.create);
 
-routes.get('/profile', ProfileController.list);
+routes.post('/ngos', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().min(10).max(11),
+    state: Joi.string().required(),
+    city: Joi.string().required().length(2),
+  })
+}), NgoController.create);
 
-routes.get('/incidents', IncidentController.list);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+}), ProfileController.list);
+
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.list);
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required().min(1),
+  }),
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}), IncidentController.delete);
 
 
 routes.get('/', (request, response) => {
@@ -24,7 +67,7 @@ routes.get('/', (request, response) => {
   return response.json({
     texto: "Olá, dev!",
     evento: "Semana Omnistack 11.0",
-    aluno: "Ana Paula Mauad"
+    aluno: "Ana"
   });
 });
 
